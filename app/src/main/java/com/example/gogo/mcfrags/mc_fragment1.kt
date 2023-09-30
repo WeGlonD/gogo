3d602256afa4802b0a7c9fc4fd0927d8800df3ae
steve <steve7941@naver.com>
2023-09-30 17:57:27 +0900

    d

--- app/src/main/java/com/example/gogo/mcfrags/mc_fragment1.kt
+++ app/src/main/java/com/example/gogo/mcfrags/mc_fragment1.kt
@@ -1,5 +1,6 @@
 package com.example.gogo.mcfrags
 
+import android.annotation.SuppressLint
 import android.app.Dialog
 import android.os.Bundle
 import android.view.Gravity
@@ -15,11 +16,13 @@ import android.widget.TextView
 import com.example.gogo.FoodItem
 import com.example.gogo.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 
 class mc_fragment1 : Fragment() {
     private lateinit var mcburger01ImageButton: ImageButton
     private val foodItem: FoodItem by lazy { FoodItem("", 0, 0, 0, 0, 0) }
-
+    private lateinit var databaseReference: DatabaseReference
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -29,14 +32,11 @@ class mc_fragment1 : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        databaseReference = FirebaseDatabase.getInstance().reference
         val view = inflater.inflate(R.layout.fragment_mc_a, container, false)
         val button = view.findViewById<Button>(R.id.gotofirst)
         button.setOnClickListener {
             activity?.finish()
-        }
-        val floatingButton = view.findViewById<FloatingActionButton>(R.id.floatingbutton)
-        floatingButton.setOnClickListener {
-            showTrayDialog(R.layout.popup_tray)
         }
         val mcBurger01 = view.findViewById<ImageButton>(R.id.mcburger01)
         mcBurger01.setOnClickListener {
@@ -81,54 +81,26 @@ class mc_fragment1 : Fragment() {
 
             if (foodItem.name.isEmpty()) {
                 foodItem.name += nameValue
-            } else {
-                foodItem.name += " $nameValue"
             }
-            foodItem.calories += calValue
-            foodItem.carbohydrates += chValue
-            foodItem.protein += prValue
-            foodItem.fat += ftValue
-            foodItem.natrium += ntValue
+            if (foodItem.calories == 0) {
+                foodItem.calories += calValue
+            }
+            if (foodItem.carbohydrates == 0) {
+                foodItem.carbohydrates += chValue
+            }
+            if (foodItem.protein == 0){
+                foodItem.protein += prValue
+            }
+            if (foodItem.fat == 0){
+                foodItem.fat += ftValue
+            }
+            if (foodItem.natrium == 0) {
+                foodItem.natrium += ntValue
+            }
+
+            val foodItemRef = databaseReference.child("Mc").push()
+            foodItemRef.setValue(foodItem)
         }
-        dialog.show()
-    }
-    private fun showTrayDialog(dialogLayoutId: Int) {
-        val dialogView = LayoutInflater.from(requireContext()).inflate(dialogLayoutId, null)
-
-        val dialog = Dialog(requireContext())
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialog.setContentView(dialogView)
-
-        val layoutParams = WindowManager.LayoutParams().apply {
-            copyFrom(dialog.window?.attributes)
-            width = WindowManager.LayoutParams.MATCH_PARENT
-            height = WindowManager.LayoutParams.WRAP_CONTENT
-            gravity = Gravity.CENTER
-        }
-        dialog.window?.attributes = layoutParams
-
-        val closeButton = dialogView.findViewById<Button>(R.id.closeButton)
-        closeButton.setOnClickListener {
-            dialog.dismiss()
-        }
-
-        val namesTextView = dialogView.findViewById<TextView>(R.id.namesTextView)
-        namesTextView.text = "선택한 음식: " + foodItem.name
-
-        val caloriesTextView = dialogView.findViewById<TextView>(R.id.caloriesTextView)
-        caloriesTextView.text = "칼로리: " + foodItem.calories + "kcal" + " (일일 권장량의 " + (100*foodItem.calories.toFloat()/2500).toInt() + "%)"
-
-        val carbohytratesTextView = dialogView.findViewById<TextView>(R.id.carbohydratesTextView)
-        carbohytratesTextView.text = "탄수화물: " + foodItem.carbohydrates + "g" + " (일일 권장량의 " + (100*foodItem.carbohydrates.toFloat()/130).toInt() + "%)"
-
-        val proteinsTextView = dialogView.findViewById<TextView>(R.id.proteinsTextView)
-        proteinsTextView.text = "단백질: " + foodItem.protein + "g" + " (일일 권장량의 " + (100*foodItem.protein.toFloat()/55).toInt() + "%)"
-
-        val fatsTextView = dialogView.findViewById<TextView>(R.id.fatsTextView)
-        fatsTextView.text = "지방: " + foodItem.fat + "g" + " (일일 권장량의 " + (100*foodItem.fat.toFloat()/51).toInt() + "%)"
-
-        val natriumsTextView = dialogView.findViewById<TextView>(R.id.natriumsTextView)
-        natriumsTextView.text = "나트륨: " + foodItem.natrium + "mg" + " (일일 권장량의 " + (100*foodItem.natrium.toFloat()/1500).toInt() + "%)"
         dialog.show()
     }
 }
