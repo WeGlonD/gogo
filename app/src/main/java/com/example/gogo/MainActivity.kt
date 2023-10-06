cd9d26908283825cd46413cff54e3d04e60d1f28
ws19999 <stardino920@naver.com>
2023-10-06 18:56:19 +0900

    로그아웃만듦

--- app/src/main/java/com/example/gogo/MainActivity.kt
+++ app/src/main/java/com/example/gogo/MainActivity.kt
@@ -4,10 +4,14 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import com.example.gogo.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : ComponentActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var auth: FirebaseAuth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +25,12 @@ class MainActivity : ComponentActivity() {
 
         binding.button02.setOnClickListener {
             val intent = Intent(this, cartActivity::class.java)
+            startActivity(intent)
+        }
+        binding.button03.setOnClickListener {
+            auth = Firebase.auth
+            auth.signOut();
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
         }
     }
